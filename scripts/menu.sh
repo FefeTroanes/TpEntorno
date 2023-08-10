1c75ee52aed80e385bb879cf915ef6abc4f98178
FefeTroanes <ftroanes@gmail.com>
2023-08-10 11:17:18 -0300

    feat docker: docker-compose created
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+touch ./datasets/caca.txt
+
 PS3="Elija una tarea : "
 OPCIONES=( "Generar imágenes" "Descomprimir imágenes" "Procesar imágenes" "Comprimir imágenes" "Salir" )
 select OPT in "${OPCIONES[@]}"
 do
   case $OPT in
     "Generar imágenes")
-      bash generar.sh
+      bash ./scripts/generar.sh
       break
       ;;
     "Descomprimir imágenes")
-      bash descomprimir.sh
+      bash ./scripts/descomprimir.sh
       break
       ;;
     "Procesar imágenes")
-      bash procesar.sh
+      bash ./scripts/procesar.sh
       break
       ;;
     "Comprimir imágenes")
-      bash comprimir.sh
+      bash ./scripts/comprimir.sh
       break
       ;;
     "Salir")
